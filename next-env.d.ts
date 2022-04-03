9ef94ea18030c84980296e4e2e843a9987aeb8b4
ryuhei-kashi <manchester.ryu.121@gmail.com>
2022-04-03 18:26:59 +0900

    setting: TypeScriptとuseSWRを導入

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// 三つのスラッシュは、Triple-Slash Directives と呼ばれているそうで、
+// コメントではなく、コンパイラへの指示として読み込まれるらしい。
+// ここでは、コンパイルするファイル群のPATHを指定している。
